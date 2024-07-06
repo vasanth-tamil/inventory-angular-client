b475bba370f5d68135e31ed3cac636aaee11ad95
vasanth <vasanthvk410@gmail.com>
2024-07-06 16:12:44 +0530

    bulma integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'inventory-web-client';
+  title = 'inventory web client';
 }
