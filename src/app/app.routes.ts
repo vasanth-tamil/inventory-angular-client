5e8b6cddf26f0c2264e8ea02e5855155265e7e7e
vasanth <vasanthvk410@gmail.com>
2024-07-07 09:36:05 +0530

    page route and testing completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SignInComponent},
+    { path: 'sign-up', component: SignUpComponent},
+    { path: 'inventory', component: InventoryComponent},
+];
